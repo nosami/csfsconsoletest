ff42bc7c6df0fb2571c522382a3d126a5210e799
nosami <jasonimison@gmail.com>
2016-09-30 11:53:52 +0100

    commit

---
+++ csconsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csconsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var class1 = new fslib.Class1();
+			Console.WriteLine(class1.X);
+
+		}
+	}
+}
